c43092e5098ee9472abc00e474378fe3e298f41c
黄彦棋 <seer@MacBook-Pro.local>
2019-03-17 10:13:28 +0800

    SRAirViews

---
+++ SRPin/SRPin/SRAirView/SRAirViews.swift
@@ -0,0 +1,19 @@
+//
+//  SRAirView.swift
+//  SRPin
+//
+//  Created by 黄彦棋 on 2019/3/17.
+//  Copyright © 2019 Seer. All rights reserved.
+//
+
+import UIKit
+
+open class SRAirView: UIView {
+
+    public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let view = super.hitTest(point, with: event)
+        return view == self ? nil : view
+    }
+}
+
+open class SRAirScrollView:UIScrollView
